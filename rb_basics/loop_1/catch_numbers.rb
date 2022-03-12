f15331449208f26f3cc9fbebfdd8e48d416b4fdd
Vietbe <vietsmall21112000@gmail.com>
2022-03-12 15:04:37 +0700

    Evenorodd

---
+++ rb_basics/loop_1/catch_numbers.rb
@@ -0,0 +1,6 @@
+loop do
+  number = rand(10)
+  puts number
+  break if number > 9
+  
+end
--- rb_basics/loop_1/even_odd.rb
+++ rb_basics/loop_1/even_odd.rb
@@ -2,11 +2,12 @@ count = 1
 
 loop do
   if count % 2 == 1 
-    puts "#{count} is odd! "
-  end
-  if count % 2 == 0
+    puts "#{count} is odd!"
+  else
     puts "#{count} is even!"
   end
   count += 1
-  break if count > 5
+  if count > 5
+    break
+  end
 end
